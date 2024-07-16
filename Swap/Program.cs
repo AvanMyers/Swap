a8c08d6c9de13280cca46509ff3587c0b82cce18
AvanMyers <smail1341@gmail.com>
2024-07-16 12:56:18 +0300

    go

---
+++ Swap/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string capOfCoffee = "Tea";
+            string capOfTea = "Coffee";
+            string cap;
+
+            Console.WriteLine($"Чашка с кофе {capOfCoffee}, Чашка чая {capOfTea}");
+
+            cap = capOfCoffee;
+            capOfCoffee = capOfTea;
+            capOfTea = cap;
+
+            Console.WriteLine($"Чашка с кофе {capOfCoffee}, Чашка чая {capOfTea}");
+            Console.ReadKey();
+        }
+    }
+}
